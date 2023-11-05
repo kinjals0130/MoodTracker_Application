cac5e50d4aa3d23552cc1051b9f4e4c0e7f36007
kinjals0130 <73653330+kinjals0130@users.noreply.github.com>
2023-11-05 18:57:25 -0500

    updating gradle compile

--- app/src/main/java/com/example/moodtracker_application/MainActivity.java
+++ app/src/main/java/com/example/moodtracker_application/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.moodtracker_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_newEntry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_newEntry = findViewById(R.id.btn_newEntry);
+
+    }
+
+    //this is called in the xml for the new entry button
+    public void newEntryActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/moodtracker_application/NoteActivity.java
+++ app/src/main/java/com/example/moodtracker_application/NoteActivity.java
@@ -2,13 +2,32 @@ package com.example.moodtracker_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NoteActivity extends AppCompatActivity {
+
+    Button btn_discard, btn_save;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        btn_discard = findViewById(R.id.btn_discardNote);
+        btn_save = findViewById(R.id.btn_saveNote);
+
+    }
+
+    //this method is called when discord button is clicked, set in xml file
+    public void mainActivity(View view){
+        Intent mainPageIntent = new Intent(NoteActivity.this, MainActivity.class);
+        startActivity(mainPageIntent);
+
     }
 }
