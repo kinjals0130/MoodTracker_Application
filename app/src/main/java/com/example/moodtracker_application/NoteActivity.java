f07aa8b4ff6d079572a2b3a1bf997ab2c35b98ef
kinjals0130 <73653330+kinjals0130@users.noreply.github.com>
2023-11-05 20:21:54 -0500

    updating classes and layout xml files

--- app/src/main/java/com/example/moodtracker_application/NoteActivity.java
+++ app/src/main/java/com/example/moodtracker_application/NoteActivity.java
@@ -2,16 +2,22 @@ package com.example.moodtracker_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.util.Date;
 
 public class NoteActivity extends AppCompatActivity {
 
-    Button btn_discard, btn_save;
-
+    Button btn_discard, btn_save, btn_rate1, btn_rate2, btn_rate3, btn_rate4, btn_rate5, btn_voiceNote;
+    EditText et_date, et_description, et_name;
+    CheckBox cb_private;
 
 
     @Override
@@ -21,6 +27,19 @@ public class NoteActivity extends AppCompatActivity {
 
         btn_discard = findViewById(R.id.btn_discardNote);
         btn_save = findViewById(R.id.btn_saveNote);
+        btn_voiceNote = findViewById(R.id.btn_voiceNote);
+        btn_rate1 = findViewById(R.id.btn_rate1);
+        btn_rate2 = findViewById(R.id.btn_rate2);
+        btn_rate3 = findViewById(R.id.btn_rate3);
+        btn_rate4 = findViewById(R.id.btn_rate4);
+        btn_rate5 = findViewById(R.id.btn_rate5);
+
+        et_date = findViewById(R.id.et_date);
+        et_description = findViewById(R.id.et_noteDescription);
+        et_name = findViewById(R.id.et_Name);
+
+        cb_private = findViewById(R.id.cb_privateEntry);
+
 
     }
 
