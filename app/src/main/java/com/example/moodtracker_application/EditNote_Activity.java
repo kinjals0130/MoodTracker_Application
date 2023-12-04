a70311aa363666c9816119b244a05803a776f742
kinjals0130 <73653330+kinjals0130@users.noreply.github.com>
2023-12-04 12:45:49 -0500

    updated edit note activity

--- app/src/main/java/com/example/moodtracker_application/EditNote_Activity.java
+++ app/src/main/java/com/example/moodtracker_application/EditNote_Activity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class EditNote_Activity extends AppCompatActivity {
 
-    EditText et_title, et_description, et_date;
+    EditText et_title, et_description, et_colour;
+
+    TextView tv_date;
     CheckBox cb_private;
 
+    LinearLayout entry_layout;
     SQLiteManager sqLiteManager;
     MyModel myModel;
 
@@ -21,8 +26,11 @@ public class EditNote_Activity extends AppCompatActivity {
 
         et_title = findViewById(R.id.et_Title);
         et_description = findViewById(R.id.et_noteDescription);
-        et_date = findViewById(R.id.et_date);
+        et_colour  = findViewById(R.id.et_colour);
+        tv_date = findViewById(R.id.DateText);
         cb_private = findViewById(R.id.cb_privateEntry);
+
+        entry_layout = findViewById(R.id.linearLayout_noteDetails);
 
         sqLiteManager = new SQLiteManager(this);
 
@@ -30,8 +38,10 @@ public class EditNote_Activity extends AppCompatActivity {
         if(myModel.getId() != null){
             et_title.setText(myModel.getTitle());
             et_description.setText(myModel.getDescription());
-            et_date.setText(myModel.getDate().toString());
+            tv_date.setText(myModel.getDate());
+            et_colour.setText(myModel.getColour());
 
+            entry_layout.setBackgroundColor(Integer.parseInt(et_colour.toString()));
         }
 
     }
